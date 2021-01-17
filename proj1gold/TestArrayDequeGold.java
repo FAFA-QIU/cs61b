9ab99f9b68e2aad7bc7389d2966576be0513efd8
FAFA-QIU <877646307@qq.com>
2021-01-17 20:24:26 +0800

    Third upload

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,45 +1,114 @@
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class TestArrayDequeGold {
     @Test
-    public void studentArrayTest() {
-        StudentArrayDeque<Integer> deque = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<>();
-        String message = "\n";
-        for (int i = 0; i < 1000; i++) {
-            int num = StdRandom.uniform(4);
-            int value = StdRandom.uniform(10);
-            if (num == 0) {
-                deque.addFirst(value);
-                ad.addFirst(value);
-                message += String.format("deque.addFirst(%d)\n", value);
-                message += "deque.get(0)\n";
-                assertEquals(message, ad.getFirst(), deque.get(0));
-            } else if (num == 1) {
-                deque.addLast(value);
-                message += String.format("deque.addLast(%d)\n", value);
-                ad.addLast(value);
-                message += "deque.get(size() - 1)\n";
-                message += "deque.size()\n";
-                assertEquals(message, ad.getLast(), deque.get(deque.size() - 1));
-            } else if (num == 2) {
-                if (!ad.isEmpty() && !deque.isEmpty()) {
-                    message += "deque.removeFirst()\n";
-                    assertEquals(message, ad.removeFirst(), deque.removeFirst());
-                } else {
-                    message += "deque.isEmpty()\n";
-                    assertTrue(message, deque.isEmpty());
-                }
-            } else {
-                if (!ad.isEmpty() && !deque.isEmpty()) {
-                    message += "deque.removeLast()\n";
-                    assertEquals(message, ad.removeLast(), deque.removeLast());
-                } else {
-                    message += "deque.isEmpty()\n";
-                    assertTrue(message, deque.isEmpty());
-                }
-            }
+    public void testArratDeque() {
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        // addLast
+        for (int i=0; i<10; i++) {
+            int random = StdRandom.uniform(100);
+            ads.addLast(random);
+            sad.addLast(random);
         }
+        for (int i=0; i<10; i++) {
+            int actual = ads.get(i);
+            int expected = sad.get(i);
+            assertEquals("Oh noooo!\nThis is bad in addLast():\n   Random number " + actual
+                            + " not equal to " + expected + "!",
+                    expected, actual);
+        }
+
+        // addFirst
+        for (int i=0; i<10; i++) {
+            int random = StdRandom.uniform(100);
+            ads.addFirst(random);
+            sad.addFirst(random);
+        }
+        for (int i=0; i<10; i++) {
+            int actual = ads.get(i);
+            int expected = sad.get(i);
+            assertEquals("Oh noooo!\nThis is bad in addFirst():\n   Random number " + actual
+                            + " not equal to " + expected + "!",
+                    expected, actual);
+        }
+
+        // removeFirst
+        List<Integer> actualList = new ArrayList<>();
+        List<Integer> expectedList = new ArrayList<>();
+        for (int i=0; i<10; i++) {
+            actualList.add(ads.removeFirst());
+            expectedList.add(sad.removeFirst());
+        }
+        for (int i=0; i<10; i++) {
+            int actual = ads.get(i);
+            int expected = sad.get(i);
+            assertEquals("Oh noooo!\nThis is bad in removeFirst():\n   Random number " + actual
+                            + " not equal to " + expected + "!",
+                    expected, actual);
+        }
+        for (int i=0; i<10; i++) {
+            int actual = actualList.get(i);
+            int expected = expectedList.get(i);
+            assertEquals("Oh noooo!\nThis is bad in removeFirst():\n   Random number " + actual
+                            + " not equal to " + expected + "!",
+                    expected, actual);
+        }
+
+
+        // removeLast
+        actualList.clear();
+        expectedList.clear();
+        for (int i=0; i<10; i++) {
+            actualList.add(ads.removeLast());
+            expectedList.add(sad.removeLast());
+        }
+        int actual = ads.size();
+        int expected = sad.size();
+        assertEquals("Oh noooo!\nThis is bad in removeLast():\n   actual size " + actual
+                        + " not equal to " + expected + "!",
+                expected, actual);
+        for (int i=0; i<10; i++) {
+            assertEquals("Oh noooo!\nThis is bad in removeLast():\n   Random number " + actualList.get(i)
+                            + " not equal to " +  expectedList.get(i) + "!",
+                    expectedList.get(i), actualList.get(i));
+        }
+
     }
+
+
+    @Test
+    public void testArratDeque2() {
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        int random = StdRandom.uniform(100);
+        ads.addFirst(random);
+        sad.addFirst(random);
+        assertEquals("addFirst("+random+")", ads.get(0), sad.get(0));
+        System.out.println("addFirst("+random+")");
+
+        random = StdRandom.uniform(100);
+        ads.addLast(random);
+        sad.addLast(random);
+        assertEquals("addLast("+random+")", ads.get(1), sad.get(1));
+        System.out.println("addLast("+random+")");
+
+        int actual = ads.removeFirst();
+        int expected = ads.removeFirst();
+        assertEquals("removeFirst()", actual, expected);
+        System.out.println("removeFirst()");
+
+        actual = ads.removeLast();
+        expected = sad.removeLast();
+        assertEquals("removeLast()", actual, expected);
+        System.out.println("removeLast()");
+    }
+
+
 }
