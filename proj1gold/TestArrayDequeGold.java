0683e3de96bd25ef99079d3c6c217c1b89e27f5d
FAFA-QIU <877646307@qq.com>
2021-01-17 19:40:07 +0800

    Fourth upload

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -13,24 +13,24 @@ public class TestArrayDequeGold {
             if (num == 0) {
                 deque.addFirst(value);
                 ad.addFirst(value);
-                message += String.format("deque.addFirst(%d)\n", value);
-                message += "deque.get(0)\n";
+                message += String.format("addFirst(%d)\n", value);
+                message += "get(0)\n";
                 assertEquals(message, ad.get(0), deque.get(0));
             } else if (num == 1) {
                 deque.addLast(value);
-                message += String.format("deque.addLast(%d)\n", value);
+                message += String.format("addLast(%d)\n", value);
                 ad.addLast(value);
-                message += "deque.get(deque.size() - 1)\n";
-                message += "deque.size()\n";
+                message += "get(deque.size() - 1)\n";
+                message += "size()\n";
                 assertEquals(message, ad.get(ad.size() - 1), deque.get(deque.size() - 1));
             } else if (num == 2) {
                 if (!ad.isEmpty() && !deque.isEmpty()) {
-                    message += "deque.removeFirst()\n";
+                    message += "removeFirst()\n";
                     assertEquals(message, ad.removeFirst(), deque.removeFirst());
                 }
             } else {
                 if (!ad.isEmpty() && !deque.isEmpty()) {
-                    message += "deque.removeLast()\n";
+                    message += "removeLast()\n";
                     assertEquals(message, ad.removeLast(), deque.removeLast());
                 }
             }
