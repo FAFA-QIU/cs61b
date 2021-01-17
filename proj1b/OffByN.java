718469851c5557120473ac017d95f988d8a362b2
FAFA-QIU <877646307@qq.com>
2021-01-17 16:49:14 +0800

    First upload

---
+++ proj1b/OffByN.java
@@ -0,0 +1,12 @@
+public class OffByN implements CharacterComparator{
+    private int gap;
+    public OffByN(int n) {
+        gap = n;
+    }
+
+    /** Returns true if characters are equal by the rules of the implementing class. */
+    @Override
+    public boolean equalChars(char x, char y) {
+        return Math.abs(x - y) == gap;
+    }
+}
---
+++ proj1b/Palindrome.java
@@ -0,0 +1,45 @@
+public class Palindrome {
+
+    // Return a Deque where the characters appear in the same order as in the String
+    public Deque<Character> wordToDeque(String word) {
+        if (word == null) {
+            return null;
+        }
+        Deque<Character> deque = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+        return deque;
+    }
+
+    // Return true if the given word is a palindrome, and false otherwise
+    public boolean isPalindrome(String word) {
+        return isPalindromeHelper(wordToDeque(word));
+    }
+
+    private boolean isPalindromeHelper(Deque<Character> deque) {
+        if (deque.size() <= 1) {
+            return true;
+        }
+        if (deque.removeFirst() != deque.removeLast()) {
+            return false;
+        }
+        return isPalindromeHelper(deque);
+    }
+
+    // return true if the word is a palindrome according to the character
+    // comparison test provided by the CharacterComparator passed in as argument cc
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        return isPalindromeHelper(wordToDeque(word), cc);
+    }
+
+    private boolean isPalindromeHelper(Deque<Character> deque, CharacterComparator cc) {
+        if (deque.size() <= 1) {
+            return true;
+        }
+        if (!cc.equalChars(deque.removeFirst(), deque.removeLast())) {
+            return false;
+        }
+        return isPalindromeHelper(deque, cc);
+    }
+}
