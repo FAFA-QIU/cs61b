4f48ba65719babea93b5bc0b069c858b3702ba6b
FAFA-QIU <877646307@qq.com>
2021-01-18 17:32:41 +0800

    Finished hw1

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -3,4 +3,14 @@ package synthesizer;
 public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     protected int fillCount;
     protected int capacity;
+
+    // Return capacity
+    public int capacity() {
+        return capacity;
+    }
+
+    // Return fillCount
+    public int fillCount() {
+        return fillCount;
+    }
 }
